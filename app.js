795b9c6e67b395b984ee70053192aee6510ae831
rakesh674003-wq <rakesh674003@gmail.com>
2026-05-31 10:42:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 // =========================
-// FIREBASE IMPORTS
+// FIREBASE IMPORTS v2
 // =========================
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
